f5d91bce8fd085fb30910f74079d88a26cc09152
ramikh96 <ramikh.developer@gmail.com>
2020-10-06 02:18:07 +0200

    remove public solution

--- ContainsDuplicate/checkForDuplicates.py
+++ ContainsDuplicate/checkForDuplicates.py
@@ -1,12 +1,11 @@
 # time: o(n)
 # space: o(n)
 
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        characterFrequency = {}
-        for num in nums:
-            if num not in characterFrequency:
-                characterFrequency[num] = 1
-            else:
-                return True
-        return False
+def containsDuplicate(self, nums: List[int]) -> bool:
+    characterFrequency = {}
+    for num in nums:
+        if num not in characterFrequency:
+            characterFrequency[num] = 1
+        else:
+            return True
+    return False
